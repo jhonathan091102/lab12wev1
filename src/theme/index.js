5825518cbb0d0cc5872cfa3bdc1c365aa5ba09db
jhonathangiron <jhonathan.giron@tecsup.edu.pe>
2021-11-14 22:30:03 -0500

    first commit

---
+++ src/theme/index.js
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  // colores
+  // fuentes
+  // espacios
+  // sombres
+  palette: {
+    primary: {
+      light: "#FB9204",
+      main: "#E8C309",
+      dark: "#E86009",
+    },
+    secondary: {
+      light: "#0EBAE8",
+      main: "#0E9FDF",
+      dark: "#0ED9DF",
+    },
+  },
+});
+
+export default theme;
---
+++ src/views/Home/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Grid } from "@mui/material";
+import { HistoryCard } from "../../components";
+
+const Home = () => {
+  return (
+    <Container maxWidth="xl">
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <HistoryCard />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <HistoryCard />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <HistoryCard />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <HistoryCard />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Home;
